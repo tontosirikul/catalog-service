aff9997cf4d7c27b57d49624bfffdf442eb0fbf7
Tanhapon Tosirikul <2781155T@student.gla.ac.uk>
2025-06-22 19:38:32 +0700

    add book domain

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -1,4 +1,4 @@
-package com.polarbookshop.catalog_service;
+package com.polarbookshop.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.polarbookshop.catalog_service;
+package com.polarbookshop.catalogservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
